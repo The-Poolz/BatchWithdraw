7283fe48f9d66b3225bbb2edf3f4be409d5f2b85
Andrew Dmytrenko <dmitrenkoae3@gmail.com>
2024-02-09 12:18:17 +0200

    split contract logic

--- contracts/BatchWithdraw.sol
+++ contracts/BatchWithdraw.sol
@@ -1,14 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "./interfaces/INFTView.sol";
+import "./BatchWithdrawInternal.sol";
 
-contract BatchWithdraw {
-    ILockDealNFTViews public lockDealNFT;
-
-    constructor(ILockDealNFTViews _lockDealNFT) {
-        lockDealNFT = _lockDealNFT;
-    }
+contract BatchWithdraw is BatchWithdrawInternal{
+    ///@dev delegate constructor to BatchWithdrawInternal 
+    constructor(ILockDealNFTViews _lockDealNFT) BatchWithdrawInternal(_lockDealNFT) {}
 
     ///@dev Use this function to withdraw the tokens from the user
     ///@param tokenIds - array of token ids - must be owned by the user
@@ -48,44 +45,5 @@ contract BatchWithdraw {
             uint256 token = lockDealNFT.tokenOfOwnerByIndex(msg.sender, --i, tokenFilter);
             _withdrawIfNoEmpty(isApproved, token);
         }
-    }
-
-    function _checkData(bool isApproved, uint256 poolId) internal view {
-        require(lockDealNFT.ownerOf(poolId) == msg.sender, "BatchWithdraw: not owner");
-        require(isApproved || lockDealNFT.getApproved(poolId) == address(this), "BatchWithdraw: not approved");
-    }
-
-    function _getIsApproved() internal view returns (bool isApproved) {
-        isApproved = lockDealNFT.isApprovedForAll(msg.sender, address(this));
-    }
-
-    function _withdraw(bool isApproved, uint256 poolId) internal {
-        _checkData(isApproved, poolId);
-        _withdraw(poolId);
-    }
-
-    function _withdraw(uint256 poolId) internal {
-        lockDealNFT.safeTransferFrom(msg.sender, address(lockDealNFT), poolId); // transfer to lockDealNFT = withdraw
-    }
-
-    function _refund(uint256 poolId) internal returns (uint256 newPoolId) {
-        IProvider refundProvider = _getProvider(poolId);
-        require(_isRefundProvider(refundProvider), "BatchWithdraw: must be RefundProvider");
-        newPoolId = lockDealNFT.totalSupply();
-        lockDealNFT.safeTransferFrom(msg.sender, address(refundProvider), poolId); // transfer to refundProvider = refund
-    }
-
-    function _withdrawIfNoEmpty(bool isApproved, uint256 poolId) internal {
-        if (lockDealNFT.getWithdrawableAmount(poolId) > 0) {
-            _withdraw(isApproved, poolId);
-        }
-    }
-
-    function _isRefundProvider(IProvider provider) internal view returns (bool isRefundProvider) {
-        isRefundProvider = keccak256(bytes(provider.name())) == keccak256(bytes("RefundProvider"));
-    }
-
-    function _getProvider(uint256 poolId) internal view returns (IProvider provider) {
-        provider = lockDealNFT.poolIdToProvider(poolId);
     }
 }
